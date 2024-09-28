ed3fb954aa27534e57c780d409a24be8a9413baf
Mehraan Azhar <mehraanazharmir@gmail.com>
2024-09-28 19:53:13 +0530

    Added an encryption feature to passwords.txt

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,17 +1,45 @@
+from cryptography.fernet import Fernet #module that allows us to encrypt text
+
+'''def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+write_key()'''
+
+def load_key():
+    f = open("key.key", "rb")
+    key = f.read()
+    f.close() #closing file to avoid errors
+    return key
+
+
 master_pwd = input("Enter Master Password: ")
+
+while True:
+    if master_pwd == 'pass@123':
+        break
+    else:
+        print("Wrong Pass")
+        quit()
+
+key = load_key()
+fer = Fernet(key)
+
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip() #rstrip is used to get rid of the \n read in
             user, pas = data.split(' | ')
-            print(f"User: {user} and Pass: {pas}")
+            dec_pas = fer.decrypt(pas.encode()).decode()
+            print(f"User: {user} and Pass: {dec_pas}")
+
 def add():
     name = input("Account Name: ")
     pwd = input('Password: ')
     #open file using with (automatically closes) in append mode
     with open('passwords.txt', 'a') as f:
-        f.write(name + ' | ' + pwd + ' \n')
+        f.write(name + ' | ' + fer.encrypt(pwd.encode()).decode() + ' \n')
         
 
 
